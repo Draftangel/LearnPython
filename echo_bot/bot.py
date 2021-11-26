7e5f5b8f7c3d11196638060ff1d5db83d918fccc
Fedor Savoyshchenko <f.savoyshchenko@corp.mail.ru>
2021-11-26 18:43:56 +0300

    Lesson2: code-review

--- echo_bot/bot.py
+++ echo_bot/bot.py
@@ -4,79 +4,62 @@ from datetime import date
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
-from variables import PLANETS
+from variables import PLANETS, INFO_TEXT, SOLAR_STRUCTURE_TEXT, HELLO_TEXT
 
 logging.basicConfig(filename="bot.log", level=logging.INFO)
 
 
 def hello(update, context):
-    print("Call method /start")
     logging.info("Call method /start")
 
-    message = '''
-{username}, тебя приветствует бот!
-Чтобы узнать возможности бота введи команду /help
-    '''.format(username=update.message.chat.username)
-
-    update.message.reply_text(message)
+    update.message.reply_text(HELLO_TEXT.format(username=update.message.chat.username))
 
 
 def info(update, context):
-    print("Call method /help")
     logging.info("Call method /help")
 
-    command_list = '''
-Данный бот является подпольной разработкой и имеет следующий набор команд:
-/start - выводит приветственное сообщение
-/help - выводит список доступных команд
-/planet [имя_планеты] - возвращает название созвездия (eng), где в данный момент находится указанная планета Солнечной системы
-    '''
-
-    update.message.reply_text(command_list)
+    update.message.reply_text(INFO_TEXT)
 
 
 def get_constellation_by_planet(update, context):
-    print("Call method /planet")
     logging.info("Call method /planet")
 
     today = date.today()
     command_words = update.message.text.split()  # /planet Mars
 
-    solar_structure_message = '''\n
-⚠️ Текущая структура Солнечной системы (за исключением Земли (Earth)) следующая:
-    - Mercury (Меркурий)
-    - Venus (Венера)
-    - Mars (Марс)
-    - Jupiter (Юпитер)
-    - Saturn (Сатурн)
-    - Uranus (Уран)
-    - Neptune (Нептун)
-    '''
-
     if len(command_words) < 2:
-        message = "‼️ Вы не указали название планеты!" + solar_structure_message
-    elif len(command_words) > 2:
-        message = "‼️ Вы указали слишком много данных! Нужно указать только название планеты"
-    else:
-        planet = command_words[1].lower()
-        if planet not in PLANETS:
-            message = "‼️ Вы указали неверное название планеты!" + solar_structure_message
-        else:
-            if planet in ["sun", "солнце"]:
-                message = "Солнце ☀️ - это звезда, а не планета"
-            elif planet in ["земля", "earth"]:
-                message = "Вы находитесь как раз на Земле 🌎"
-            elif planet in ["луна", "moon"]:
-                message = "Луна 🌛 является спутником Земли 🌎"
-            elif planet in ["плутон", "pluto"]:
-                message = "Плутон, к сожалению, с 2006 года не является планетой Солнечной системы"
-            else:
-                planet_api = getattr(ephem, PLANETS[planet])(today.strftime("%Y/%m/%d"))
-                (_, constellation_name) = ephem.constellation(planet_api)
+        update.message.reply_text("‼️ Вы не указали название планеты!" + SOLAR_STRUCTURE_TEXT)
+        return
 
-                message = f"Сегодня планета {planet.capitalize()} находится в созвездии {constellation_name}"
+    if len(command_words) > 2:
+        update.message.reply_text("‼️ Вы указали слишком много данных! Нужно указать только название планеты")
+        return
 
-    update.message.reply_text(message)
+    planet = command_words[1].lower()
+    if planet not in PLANETS:
+        update.message.reply_text("‼️ Вы указали неверное название планеты!" + SOLAR_STRUCTURE_TEXT)
+        return
+
+    if planet in ["sun", "солнце"]:
+        update.message.reply_text("Солнце ☀️ - это звезда, а не планета")
+        return
+
+    if planet in ["земля", "earth"]:
+        update.message.reply_text("Вы находитесь как раз на Земле 🌎")
+        return
+
+    if planet in ["луна", "moon"]:
+        update.message.reply_text("Луна 🌛 является спутником Земли 🌎")
+        return
+
+    if planet in ["плутон", "pluto"]:
+        update.message.reply_text("Плутон, к сожалению, с 2006 года не является планетой Солнечной системы")
+        return
+
+    planet_api = getattr(ephem, PLANETS[planet])(today.strftime("%Y/%m/%d"))
+    _, constellation_name = ephem.constellation(planet_api)
+
+    update.message.reply_text(f"Сегодня планета {planet.capitalize()} находится в созвездии {constellation_name}")
 
 
 def echo(update, context):
